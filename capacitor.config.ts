09ff5f69d537e53f06f15f1a21b1da8c292d2e95
Ian G <yo.ian.g@gmail.com>
2026-09-17 20:08:53 +0900

    Initial commit: Vite + Solid + TypeScript + Capacitor scaffold

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.adorkable.wikiscroll',
+  appName: 'wiki-scroll',
+  webDir: 'dist'
+};
+
+export default config;
